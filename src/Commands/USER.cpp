c4b177ef976c85ce6876b0c7ae5b4942d93eb0a8
aabajyan <arsen.abajyan@pm.me>
2022-10-03 02:52:46 +0400

    [ADD] Add todos.

---
+++ src/Commands/USER.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   USER.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabajyan <arsen.abajyan@pm.me>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 02:51:42 by aabajyan          #+#    #+#             */
+/*   Updated: 2022/10/03 02:52:31 by aabajyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "User.h"
+#include "Command.h"
+#include "Server.h"
+
+void USER(Command &command) {
+  (void)command;
+  // TODO: Work on this.
+}
