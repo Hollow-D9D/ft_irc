87fe57448b2b476b713f343676ec9c299a7d3e42
aabajyan <arsen.abajyan@pm.me>
2022-10-05 15:02:52 +0400

    [FIX] Make it compile on linux.

--- src/Commands/INVITE.cpp
+++ src/Commands/INVITE.cpp
@@ -4,37 +4,38 @@
 #include "../User.h"
 
 void INVITE(Command &cmd) {
+
+  const std::vector<std::string> &arguments = cmd.get_arguments();
+  User &sender = cmd.get_sender();
+  Server &server = cmd.get_server();
+
   // Numeric error responses
-  if (cmd.get_arguments().size() < 2)
-    return cmd.get_sender().reply(461, "INVITE");
+  if (arguments.size() < 2)
+    return sender.reply(461, "INVITE");
 
-  if (cmd.get_server().get_user(cmd.get_arguments()[0]) == NULL)
-    return cmd.get_sender().reply(401, cmd.get_arguments()[1]);
+  if (server.get_user(arguments[0]) == NULL)
+    return sender.reply(401, arguments[1]);
 
-    if (cmd.get_server().get_channel(cmd.get_arguments()[0]).inChannel(cmd.get_arguments()[1]))
-        cmd.get_sender().reply(443, cmd.get_arguments()[0], cmd.get_arguments()[1]);
-    
-    // Implementation
-    if (cmd.get_server().is_channel(cmd.get_arguments()[1]))
-    {
-        Channel &channel = cmd.get_server().get_channel(cmd.get_arguments()[1]);
-        if (channel.getChannelMode().find('i') != std::string::npos &&
-            channel.getUserMode(cmd.get_sender()).find('o') != std::string::npos &&
-            !cmd.get_sender().get_user_mode().has_mode(USER_MODE_OPERATOR))
-        {
-            cmd.get_sender().reply(482, cmd.get_arguments()[1]);
-            return;
-        }
-        else if (!channel.inChannel(cmd.get_sender().get_nickname()))
-        {
-            cmd.get_sender().reply(442, cmd.get_arguments()[1]);
-            return;
-        }
-        channel.addInvitedUser(*(cmd.get_server().get_user(cmd.get_arguments()[0])));
+  if (server.get_channel(arguments[0]).inChannel(arguments[1]))
+    sender.reply(443, arguments[0], arguments[1]);
+
+  // Implementation
+  if (server.is_channel(arguments[1])) {
+    Channel &channel = server.get_channel(arguments[1]);
+    if (channel.getChannelMode().find('i') != std::string::npos &&
+        channel.getUserMode(sender).find('o') != std::string::npos &&
+        !sender.get_user_mode().has_mode(USER_MODE_OPERATOR)) {
+      sender.reply(482, arguments[1]);
+      return;
+    } else if (!channel.inChannel(sender.get_nickname())) {
+      sender.reply(442, arguments[1]);
+      return;
     }
-    cmd.get_sender().send_to(*(cmd.get_server().get_user(cmd.get_arguments()[0])), "INVITE" + cmd.get_arguments()[0] + " " + cmd.get_arguments()[1]);
-    cmd.get_sender().reply(341, cmd.get_arguments()[0], cmd.get_arguments()[1]);
-    if (cmd.get_server().get_user(cmd.get_arguments()[0])->get_user_mode().has_mode(USER_MODE_AWAY))
-        cmd.get_sender().reply(301, cmd.get_arguments()[0], AWAY_MESSAGE);
-
+    channel.addInvitedUser(*(server.get_user(arguments[0])));
+  }
+  sender.send_to(*(server.get_user(arguments[0])),
+                 "INVITE" + arguments[0] + " " + arguments[1]);
+  sender.reply(341, arguments[0], arguments[1]);
+  if (server.get_user(arguments[0])->get_user_mode().has_mode(USER_MODE_AWAY))
+    sender.reply(301, arguments[0], AWAY_MESSAGE);
 }
