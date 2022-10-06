387f864ead6ac66b9de017a5e8a4694863ff068d
AgnessaAvetyan <agnessa.avetyan.01@mail.ru>
2022-10-06 20:00:13 +0400

    O, o

--- src/Commands/KICK.cpp
+++ src/Commands/KICK.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   KICK.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aabajyan <arsen.abajyan@pm.me>             +#+  +:+       +#+        */
+/*   By: aavetyan <aavetyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 12:10:59 by aavetyan          #+#    #+#             */
-/*   Updated: 2022/10/06 12:44:21 by aabajyan         ###   ########.fr       */
+/*   Updated: 2022/10/06 19:57:10 by aavetyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void KICK(Command &cmd) {
     }
     Channel &channel = cmd.get_server().get_channel(*it);
     if (channel.getUserMode(cmd.get_sender()).find('O') == std::string::npos &&
-        channel.getUserMode(cmd.get_sender()).find('O') == std::string::npos &&
+        channel.getUserMode(cmd.get_sender()).find('o') == std::string::npos &&
         cmd.get_sender().get_user_mode().has_mode(USER_MODE_OPERATOR))
       return cmd.get_sender().reply(482, *it);
     std::vector<std::string>::iterator it2;
